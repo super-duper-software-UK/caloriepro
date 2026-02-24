254e7b265222acadc9ddb83dd6dabe2eca7a9686
super-duper-software-UK <pmills@rocketmail.com>
2026-02-24 13:19:21 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// This service worker immediately unregisters itself and clears all caches
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
-        .then(() => self.registration.unregister())
-    );
-});
